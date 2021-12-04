39ef8693cb1bd9cb2fe35cbfbef315ba11b06301
Harry <b9026715@ncl.ac.uk>
2021-12-04 14:24:19 +0000

    Added James' work

--- app.py
+++ app.py
@@ -44,3 +44,29 @@ def internal_error(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# James' version of app.py from original base structure
+"""
+import os
+from flask import Flask, render_template
+from users.views import users
+
+# APP CONFIGURATION
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='test'  # TODO: Configuration values here will later be put into a config.py
+)
+# BLUEPRINT REGISTRATION
+app.register_blueprint(users)
+
+
+# HOME PAGE
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+"""
---
+++ student/views.py
@@ -0,0 +1,29 @@
+# James' version of /users/views.py from original base structure
+"""
+# IMPORTS
+from flask import Blueprint, render_template
+
+# CONFIG
+users = Blueprint('users', __name__, template_folder='templates')
+
+
+# VIEWS
+@users.route('/registration', methods=['GET', 'POST'])
+def registration():
+    return render_template('auth/registration.html')
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('auth/login.html')
+
+
+@users.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@users.route('/content', methods=['GET'])
+def content():
+    return render_template('content.html')
+"""
