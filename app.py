43dab0bd1f2be2582b7c3ef488e1e9626f56983f
JamesNewsome02 <J.Newsome2@newcastle.ac.uk>
2021-12-17 01:19:59 +0000

    Moved blueprint registration

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from users.views import users
-import logging
+Moimport logging
 
 
 # app configuration
@@ -11,8 +10,6 @@ app.config.from_object('config.DevelopmentConfig')
 # create database instance
 db = SQLAlchemy(app)
 print(db)
-# blueprint registration
-app.register_blueprint(users)
 
 
 @app.route('/')
@@ -48,7 +45,10 @@ def internal_error(_error):
 
 if __name__ == '__main__':
     # Imports blueprints
+    from users.views import users
     from webadmin.views import webadmin_blueprint
+
+    app.register_blueprint(users)
     app.register_blueprint(webadmin_blueprint)
 
     app.run(debug=True)
