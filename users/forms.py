02c410752ce8c5e772b4d1596f79068c0bcc5ad2
JoopJ <joppenheimerjo@gmail.com>
2021-12-21 01:06:47 +0000

    Added quiz question page
    Added a page that displayed quiz questions and any number of radio fields as possible answers. The current functionality assumes the questions will be stored as question string and answer strings where each answer has a numerical value, used to indicate the correct answer with another stored value.

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, SelectMultipleField, widgets, RadioField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[DataRequired()])
     password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class QuizQuestion(FlaskForm):
+    question = StringField("")
+    radio_field = RadioField(validators=[DataRequired()])
     submit = SubmitField()
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 # IMPORTS
-from flask import Blueprint, render_template, redirect, flash
+from flask import Blueprint, render_template, redirect, flash, url_for
 from flask_login import login_user, current_user
+from wtforms.fields.core import Label
 from werkzeug.security import check_password_hash
+from werkzeug.datastructures import MultiDict
 
 from models import User
-from users.forms import LoginForm
+from users.forms import LoginForm, QuizQuestion
 
 # CONFIG
 users = Blueprint('users', __name__, template_folder='templates')
@@ -39,7 +41,7 @@ def login():
         login_user(user)
 
         # redirect the user to the appropriate page for their role
-        if current_user.user_type == 'teacher':     # this may need changing
+        if current_user.user_type == 'teacher':  # this may need changing
             return redirect('templates/dashboard.html')
         else:
             return redirect('templates/content.html')
@@ -61,13 +63,52 @@ def account():
 def content():
     return render_template('content.html')
 
-  
+
 @users.route('/create_group', methods=['GET'])
 def create_group():
-
     return render_template('auth/create_group.html')
-  
+
+
+# no button for this url yet so needs to be access directly through URL
+# question_id is just a proof of concept for getting the desired question and can be left as anything e.g. 01
+@users.route('/quiz_question/<question_id>', methods=['POST', 'GET'])
+def quiz_question(question_id):
+    # should probably be done with a database. Select the desired question data - ASK IN MEETING
+    # question = string
+    # choices = list or dict - ASK IN MEETING
+    # answer = int (probably) - ASK IN MEETING
+
+    # Examples
+    question = "Which of these does not fit?"
+    choices = ("1", "Dog"), ("2", "Cat"), ("3", "Fish")
+    answer = "3"
+
+    form = QuizQuestion()
+    # define the question label and answers
+    form.question.label = Label("question", question + " ID: " + question_id)
+    form.radio_field.choices = choices
+
+    if form.validate_on_submit():
+        print(form.radio_field.data)
+        if form.radio_field.data == answer:
+            print("Correct answer")
+            # handle correct answer
+            # could be held as session data then updated to database upon completion - would then have to reveal
+            #   correct answers at the end of the test to prevent cheating.
+
+        else:
+            print("Incorrect answer")
+            # handle incorrect answer
+
+        # rendering next question - question ID's could be of the format XX-YY where XX is the quiz ID and YY is
+        #   the question ID, this way we could move to the next question by simply incrementing the quiz ID. Ending
+        #   the quiz could then be indicated by the question_id returning null when requested from the database
+        next_question_id = int(question_id) + 1
+        return redirect(f"/quiz_question/{next_question_id}")
+
+    return render_template('quiz_question.html', form=form)
+
+
 """
 Useful explanation of querying with Inheritance: https://docs.sqlalchemy.org/en/14/orm/inheritance_loading.html
 """
-
