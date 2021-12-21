f5e24d871493bdbad8dd804066923cfbff0bf64b
JamesNewsome02 <J.Newsome2@newcastle.ac.uk>
2021-12-21 23:35:25 +0000

    Added Student registration

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import InputRequired, ValidationError, Length
+from flask import flash
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField
+from wtforms.validators import InputRequired, ValidationError, NumberRange
 
 
 def name_check(_form, name):
-    chars = "* ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < >"
+    chars = ["*", "?", "!", "'", "^", "+", "%", "&", "/", "(", ")", "=", "}", "]", "[", "{", "$", "#", "@", "<", ">"]
     for char in name.data:
         if char in chars:
-            raise ValidationError("Special Characters not allowed.")
+            flash("Special characters are not allowed", category="danger")
+            raise ValidationError("Name cannot include special characters.")
 
 
-class GroupRegistrationForm(FlaskForm):
+def size_check():
+    pass
+
+
+class CreateGroup(FlaskForm):
     name = StringField(validators=[InputRequired(), name_check])
-    size = IntegerField(validators=[Length(min=1, max=50)], default=50, message="Maximum group size is 50.")
+    size = IntegerField(validators=[NumberRange(min=1, max=50)], default=30)
+    key_stage = IntegerField(validators=[NumberRange(min=1, max=9)])
+    submit = SubmitField()
+
+
+class RegisterStudent(FlaskForm):
+    names = TextAreaField(validators=[InputRequired(), name_check])
+    submit = SubmitField()
