a4675a0ca776e9f4eb19ec3cdd475776275fe6ed
James-02 <james.newsome02@gmail.com>
2022-01-26 23:01:24 +0000

    Reorganised test objects and used a more secure db initialization method

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 # created 4/12/2021 by Josh Oppenheimer
 # model of the database. Has constructors for handling the database information within python
 # (Will change as we change that details of variables and table interaction)
+from app import db, app
 from flask_login import UserMixin
-from app import db
 from werkzeug.security import generate_password_hash
 
 
@@ -114,7 +114,7 @@ class StudentQuizScores(db.Model):
     id = db.Column(db.Integer(), autoincrement=True, nullable=False, primary_key=True)
     quiz_id = db.Column(db.ForeignKey('quizzes.id'))
     student_id = db.Column(db.ForeignKey('student.id'))
-    score = db.Column(db.Integer())    # percentile score
+    score = db.Column(db.Integer())  # percentile score
 
     # one (quiz) to many (scores)
     quiz = db.relationship('Quiz', foreign_keys='StudentQuizScores.quiz_id')
@@ -137,11 +137,9 @@ class KeyStage(db.Model):
         self.key_stage = key_stage
 
 
-def init_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
+def create_models():
+    """ Function to define testing instances """
+    questions, quizzes = [], []
     teacher = Teacher(role="teacher",
                       name="Adam Smith",
                       username="AdamSmith3412",
@@ -153,7 +151,7 @@ def init_db():
     group = Group(id="453153",
                   name="Class 4",
                   size=30,
-                  teacher_id=teacher.id,
+                  teacher_id=None,
                   key_stage=1)
 
     student = Student(role="student",
@@ -164,96 +162,95 @@ def init_db():
                       registered_on="19/12/2021 00:55:11",
                       group_id=group.id)
 
-    quiz0 = Quiz(name="Weather and Climate",
-                 key_stage=1)
+    quizzes.append(Quiz(name="Weather and Climate",
+                        key_stage=1))
 
-    question0 = Question(quiz_id=1,
-                         question_text="What is Weather?",
-                         choices="If it's raining or not"
-                                 "|The daily conditions in a small area"
-                                 "|The average temperature"
-                                 "|The number of clouds in the sky",
-                         correct_choice=1)
+    questions.append(Question(quiz_id=1,
+                              question_text="What is Weather?",
+                              choices="If it's raining or not"
+                                      "|The daily conditions in a small area"
+                                      "|The average temperature"
+                                      "|The number of clouds in the sky",
+                              correct_choice=1))
 
-    question1 = Question(quiz_id=1,
-                         question_text="What is Climate?",
-                         choices="The long term weather pattern over a large area"
-                                 "|The average temperature of the world"
-                                 "|The weather at the end of the year"
-                                 "|The weather over a large area",
-                         correct_choice=0)
+    questions.append(Question(quiz_id=1,
+                              question_text="What is Climate?",
+                              choices="The long term weather pattern over a large area"
+                                      "|The average temperature of the world"
+                                      "|The weather at the end of the year"
+                                      "|The weather over a large area",
+                              correct_choice=0))
 
-    question2 = Question(quiz_id=1,
-                         question_text="What is the Climate like in the United Kingdom?",
-                         choices="Cool summers and warm winters"
-                                 "|Very hot and windy all year"
-                                 "|Cool winters and warm summers"
-                                 "|Very Cold with no rain all year",
-                         correct_choice=2)
+    questions.append(Question(quiz_id=1,
+                              question_text="What is the Climate like in the United Kingdom?",
+                              choices="Cool summers and warm winters"
+                                      "|Very hot and windy all year"
+                                      "|Cool winters and warm summers"
+                                      "|Very Cold with no rain all year",
+                              correct_choice=2))
 
-    question3 = Question(quiz_id=1,
-                         question_text="What is the Climate like in the desert?",
-                         choices="Cold with lots of snowfall"
-                                 "|Hot and lots of rainfall"
-                                 "|Cold and windy"
-                                 "|Very hot and dry all year",
-                         correct_choice=3)
+    questions.append(Question(quiz_id=1,
+                              question_text="What is the Climate like in the desert?",
+                              choices="Cold with lots of snowfall"
+                                      "|Hot and lots of rainfall"
+                                      "|Cold and windy"
+                                      "|Very hot and dry all year",
+                              correct_choice=3))
 
-    question4 = Question(quiz_id=1,
-                         question_text="What is the Climate like in the rainforest",
-                         choices="Hot all year and very wet"
-                                 "|Hot and dry"
-                                 "|Cold and lots of rainfall"
-                                 "|Very hot and windy",
-                         correct_choice=0)
+    questions.append(Question(quiz_id=1,
+                              question_text="What is the Climate like in the rainforest",
+                              choices="Hot all year and very wet"
+                                      "|Hot and dry"
+                                      "|Cold and lots of rainfall"
+                                      "|Very hot and windy",
+                              correct_choice=0))
 
-    quiz1 = Quiz(name="Climate Change",
-                 key_stage=2)
+    quizzes.append(Quiz(name="Climate Change",
+                        key_stage=2))
 
-    question5 = Question(quiz_id=2,
-                         question_text="What is Climate Change?",
-                         choices="Describes the weather changing each day"
-                                 "|Describes the earth moving further away from the sun"
-                                 "|When the seasons change, for example: spring to summer"
-                                 "|Describes how the planets climates change over a long period of time ",
-                         correct_choice=3)
+    questions.append(Question(quiz_id=2,
+                              question_text="What is Climate Change?",
+                              choices="Describes the weather changing each day"
+                                      "|Describes the earth moving further away from the sun"
+                                      "|When the seasons change, for example: spring to summer"
+                                      "|Describes how the planets climates change over a long period of time ",
+                              correct_choice=3))
 
-    question6 = Question(quiz_id=2,
-                         question_text="What is Global Warming?",
-                         choices="Describes the change in the Earth's average temperature over a long period of time"
-                                 "|Describes the increase in the number of clouds around the world"
-                                 "|The cooling of the Earth and increase of rainfall"
-                                 "|Describes the increase in human body temperature over time",
-                         correct_choice=0)
+    questions.append(Question(quiz_id=2,
+                              question_text="What is Global Warming?",
+                              choices="Describes the change in the Earth's average temperature over a long period"
+                                      "|Describes the increase in the number of clouds around the world"
+                                      "|The cooling of the Earth and increase of rainfall"
+                                      "|Describes the increase in human body temperature over time",
+                              correct_choice=0))
 
-    question7 = Question(quiz_id=2,
-                         question_text="Which of these is not a greenhouse gas?",
-                         choices="Water Vapour"
-                                 "|Acid"
-                                 "|Carbon Dioxide"
-                                 "|Methane",
-                         correct_choice=1)
+    questions.append(Question(quiz_id=2,
+                              question_text="Which of these is not a greenhouse gas?",
+                              choices="Water Vapour"
+                                      "|Acid"
+                                      "|Carbon Dioxide"
+                                      "|Methane",
+                              correct_choice=1))
 
-    question8 = Question(quiz_id=2,
-                         question_text="Why is it important that the levels of greenhouse gases in our atmosphere "
-                                       "remain roughly the same?",
-                         choices="So that we can breath more easily"
-                                 "|To maintain a stable climate"
-                                 "|To help keep us cool"
-                                 "|To keep the sky blue",
-                         correct_choice=1)
+    questions.append(Question(quiz_id=2,
+                              question_text="Why is it important that the levels of greenhouse gases in our atmosphere "
+                                            "remain roughly the same?",
+                              choices="So that we can breath more easily"
+                                      "|To maintain a stable climate"
+                                      "|To help keep us cool"
+                                      "|To keep the sky blue",
+                              correct_choice=1))
 
-    question9 = Question(quiz_id=2,
-                         question_text="What is causing the sharp rise in carbon dioxide levels in our "
-                                       "atmosphere today?",
-                         choices="The Earth's natural cycle"
-                                 "|Human actions such as burning fossil fuels, farming and deforestation"
-                                 "|Sun spots and volcanic activity"
-                                 "|The sun getting hotter",
-                         correct_choice=0)
+    questions.append(Question(quiz_id=2,
+                              question_text="What is causing the sharp rise in carbon dioxide levels in our "
+                                            "atmosphere today?",
+                              choices="The Earth's natural cycle"
+                                      "|Human actions such as burning fossil fuels, farming and deforestation"
+                                      "|Sun spots and volcanic activity"
+                                      "|The sun getting hotter",
+                              correct_choice=0))
 
-    key_stage1 = KeyStage(key_stage=1)
-    key_stage2 = KeyStage(key_stage=2)
+    key_stages = [KeyStage(key_stage=1), KeyStage(key_stage=2)]
 
     student_quiz_score0 = StudentQuizScores(quiz_id=1,
                                             student_id=2,
@@ -280,28 +277,38 @@ def init_db():
     db.session.add(teacher)
     db.session.add(admin)
     db.session.commit()
+
     db.session.add(group)
     db.session.add(admin_group)
-    db.session.commit()
     db.session.add(student)
-    db.session.add(quiz0)
-    db.session.add(quiz1)
-    db.session.add(question0)
-    db.session.add(question1)
-    db.session.add(question2)
-    db.session.add(question3)
-    db.session.add(question4)
-    db.session.add(question5)
-    db.session.add(question6)
-    db.session.add(question7)
-    db.session.add(question8)
-    db.session.add(question9)
-    db.session.add(key_stage1)
-    db.session.add(key_stage2)
+
+    for key_stage in key_stages:
+        db.session.add(key_stage)
+
+    for quiz in quizzes:
+        db.session.add(quiz)
+
+    for question in questions:
+        db.session.add(question)
+
     db.session.add(student_quiz_score0)
     db.session.add(student_quiz_score1)
+
     group.teacher_id = teacher.id
     admin_group.teacher_id = admin.id
-
     db.session.commit()
 
+
+def init_db():
+    """ Database table creation using models """
+    db.init_app(app)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        create_models()
+        db.session.commit()
+        print(db, "\n===== SUCCESS ======")
+
+
+if __name__ == "__main__":
+    init_db()
