e3ee350a3f40bf086594a1d0130db1b97f16ebb2
James-02 <james.newsome02@gmail.com>
2022-01-26 23:00:41 +0000

    Added more effective app initialization method to stop Flask Server Internal Error

--- app.py
+++ app.py
@@ -7,30 +7,66 @@ from functools import wraps
 from flask import Flask, render_template, session
 from flask_login import current_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
 import logging
+
+
+db = SQLAlchemy()
 
 # app configuration
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 if app.config['ENV'] == 'production':
     app.config.from_object('config.ProductionConfig')
-
 else:
     app.config.from_object('config.DevelopmentConfig')
 
 
-# create database instance
-db = SQLAlchemy(app)
-print(db)
-
 # logging
-fh = logging.FileHandler("security.log", "w")
-fh.setLevel(logging.WARNING)
-formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%m:%S %p')
-fh.setFormatter(formatter)
-logger = logging.getLogger('')
-logger.propagate = False
-logger.addHandler(fh)
+def setup_logging(filename):
+    """ Function to setup a logging file for important actions using logging module."""
+    fh = logging.FileHandler(filename, "w")
+    fh.setLevel(logging.WARNING)
+    formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%m:%S %p')
+    fh.setFormatter(formatter)
+    logger = logging.getLogger('')
+    logger.propagate = False
+    logger.addHandler(fh)
+
+
+def setup_login():
+    """ Function to initialize the flask-login LoginManager and attributes"""
+    # Flask LoginManager instance attributes
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.refresh_view = 'auth.login'
+    login_manager.login_message_category = 'danger'
+    login_manager.login_message = 'Session timed out, please login again.'
+    login_manager.init_app(app)
+
+    from models import User
+    db.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+
+@app.before_first_request
+def execute_this():
+    setup_logging('security.log')
+    setup_login()
+
+    # import blueprints
+    from auth.views import auth
+    from users.views import users
+    from groups.views import groups
+    from webadmin.views import webadmin_blueprint
+
+    # register blueprints
+    app.register_blueprint(auth)
+    app.register_blueprint(users)
+    app.register_blueprint(groups)
+    app.register_blueprint(webadmin_blueprint)
+
 
 @app.before_request
 def before_request():
@@ -79,6 +115,7 @@ def page_forbidden(_error):
 
 def requires_roles(*roles):
     """ Function using wraps annotation to check if the current_user has the role required."""
+
     def wrapper(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
@@ -86,33 +123,11 @@ def requires_roles(*roles):
                 # Redirect the user to an unauthorised notice!
                 return render_template('errors/403.html')
             return f(*args, **kwargs)
+
         return wrapped
+
     return wrapper
 
 
 if __name__ == '__main__':
-    # Flask LoginManager instance attributes
-    login_manager = LoginManager()
-    login_manager.login_view = 'users.login'
-    login_manager.refresh_view = 'users.login'
-    login_manager.login_message_category = 'danger'
-    login_manager.login_message = 'Session timed out, please login again.'
-    login_manager.init_app(app)
-
-    from models import User
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    # import blueprints
-    from users.views import users
-    from groups.views import groups
-    from webadmin.views import webadmin_blueprint
-   
-    # register blueprints
-    app.register_blueprint(users)
-    app.register_blueprint(groups)
-    app.register_blueprint(webadmin_blueprint)
-    
     app.run(debug=True)
